4c170f6d0905e5302285b0d917534ff43818e4ea
ecaz <me@ecaz.xyz>
2022-06-29 12:35:57 -0400

    Bump dependency versions

--- pkg/setup.py
+++ pkg/setup.py
@@ -6,11 +6,11 @@ setup(
     packages=['src'] + find_packages(),
     install_requires=[
         'aiohttp >= 3.8.1',
-        'click >= 8.0.3',
+        'click >= 8.1.3',
         'pycoingecko >= 2.2.0',
         'PyYAML >= 6.0',
-        'requests >= 2.27.1',
-        'web3 >= 5.28.0',
+        'requests >= 2.28.1',
+        'web3 >= 5.29.2',
     ],
     entry_points={
         'console_scripts': [
