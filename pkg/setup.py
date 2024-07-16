226204d125ea0d93875e66cea75529041261545e
spiceworm <me@spiceworm.xyz>
2024-07-15 19:35:40 -0600

    Bump all dependency versions

--- pkg/setup.py
+++ pkg/setup.py
@@ -5,12 +5,12 @@ setup(
     author="ecaz.eth",
     packages=["src"] + find_packages(),
     install_requires=[
-        "aiohttp >= 3.8.3",
-        "click >= 8.1.3",
-        "finnhub-python >= 2.4.19",
+        "aiohttp >= 3.9.5",
+        "click >= 8.1.7",
+        "finnhub-python >= 2.4.20",
         "pycoingecko >= 3.1.0",
-        "PyYAML >= 6.0",
-        "requests >= 2.28.1",
+        "PyYAML >= 6.0.1",
+        "requests >= 2.32.3",
     ],
     entry_points={"console_scripts": ["networth = src.__main__:main"]},
 )
