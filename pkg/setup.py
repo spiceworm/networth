334ad5d2785f3a5193b6804529196fc5bfb95c5b
spiceworm <me@spiceworm.xyz>
2024-08-16 22:28:44 -0600

    Bump dependencies, remove unused dependencies

--- pkg/setup.py
+++ pkg/setup.py
@@ -6,12 +6,11 @@ setup(
     packages=["src"] + find_packages(),
     install_requires=[
         "aioetherscan >= 0.9.4",
-        "aiohttp >= 3.9.5",
+        "aiohttp >= 3.10.3",
         "click >= 8.1.7",
         "finnhub-python >= 2.4.20",
         "pycoingecko >= 3.1.0",
-        "PyYAML >= 6.0.1",
-        "requests >= 2.32.3",
+        "PyYAML >= 6.0.2",
     ],
     entry_points={"console_scripts": ["networth = src.__main__:main"]},
 )
